05d526bda19c515d2a1e7a107ba8e62d8d56f0a8
justmg <aykyassini@gmail.com>
2023-10-17 11:09:49 -0500

    Update op.py

--- op.py
+++ op.py
@@ -1,22 +1,36 @@
 import streamlit as st
 import requests
+import json
 
-# Function to query the Exclusions API
-def query_exclusions_api(uei):
-    url = f"https://api.gsa.gov/entity-information/v1/exclusions/{uei}"
+# Function to fetch exclusion data
+def fetch_exclusion_data(uei, api_key):
+    base_url = f"https://api.open.gsa.gov/exclusions/{uei}"
     headers = {
-        'Authorization': 'poORnzZwlTFovtdCgNJHvkwCyrdvD7w6I66fBmki',
+        "X-Api-Key": api_key,
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(base_url, headers=headers)
+    
     if response.status_code == 200:
         return response.json()
     else:
-        st.error(f"Failed to retrieve data: {response.text}")
+        return None
 
-# Streamlit interface
-st.title("Exclusions API Query")
-uei = st.text_input("Enter Unique Entity Identifier (UEI):")
+# Streamlit UI
+st.title("Exclusion Information Checker")
+
+# Taking UEI as input
+uei = st.text_input("Enter UEI: ")
+
 if uei:
-    data = query_exclusions_api(uei)
+    # Define your API Key here
+    api_key = "poORnzZwlTFovtdCgNJHvkwCyrdvD7w6I66fBmki" # Replace with your actual API Key
+    
+    # Fetching and displaying data
+    data = fetch_exclusion_data(uei, api_key)
+    
     if data:
-        st.write(data)
+        st.write("### Exclusion Details:")
+        st.json(data)
+    else:
+        st.write("No data found or an error occurred while fetching the data.")
+
